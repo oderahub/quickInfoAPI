0cea90a379f8fc4abe03b9d8a3d575ee330de9a1
oderahub <okere_chidera@yahoo.com>
2025-02-04 10:31:25 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+app.use(cors())
 
-const PORT = 5000
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.disable('x-powered-by')
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.status(200).json({
+    email: 'okere_chidera@yahoo.com',
+    current_time: new Date().toISOString(),
+    github_url: 'https://github.com/oderahub/quickInfoAPI'
+  })
 })
 
 app.listen(PORT, () => {
