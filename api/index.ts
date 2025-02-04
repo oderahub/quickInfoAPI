53cddc2a53bf1a969d337596fb5c68f5da9a44b5
oderahub <okere_chidera@yahoo.com>
2025-02-04 10:51:28 +0100

    vercel-setup

---
+++ api/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.disable('x-powered-by')
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    email: 'okere_chidera@yahoo.com',
+    current_time: new Date().toISOString(),
+    github_url: 'https://github.com/oderahub/quickInfoAPI'
+  })
+})
+
+export default (req: Request, res: Response) => {
+  app(req, res)
+}
